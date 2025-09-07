e9444e230180eefbcf0fdb3cf0aa1cb57a388f6b
JackDesBwa <jackdesbwa@gmail.com>
2025-09-07 18:39:38 +0200

    Add AFRAME (from js13k) and a basic scene

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
-import './style.css'
-import logo from '/catway.svg'
+function C(tag: string, props?: Record<string, string>, style?: Record<string, string|number>) {
+	const el = typeof tag == 'string' ? document.createElement(tag) : tag;
+	for (const [k, v] of Object.entries(props || {})) el.setAttribute(k, v);
+	for (const [k, v] of Object.entries(style || {})) el.style.setProperty(k, '' + v);
+	return el;
+}
 
-const img = document.createElement('img');
-Object.assign(img, {
-	width: 960,
-	height: 960,
-	src: logo,
+const scene = C('a-scene', {
+	'xr-mode-ui': 'XRMode: xr',
 });
-document.body.append(img);
+if (import.meta.env.DEV) scene.setAttribute('stats', '');
+document.body.appendChild(scene);
+
+scene.append(C('a-entity', { // Camera
+	'camera': 'fov: 30',
+	'position': '0 1 1',
+	'look-controls': '',
+	'wasd-controls': '',
+}));
+
+const assets = C('a-assets');
+scene.appendChild(assets);
+
+assets.append(C('img', { // Cat texture
+	'id': 'tex_c',
+	'src': '/catway.svg',
+}));
+
+scene.append(C('a-plane', { // Logo
+	'position': '0 1 -0.6',
+	'width': '0.4',
+	'height': '0.4',
+	'material': 'side: double; transparent: true; src: #tex_c',
+	'animation': 'property: rotation; to: 0 360 0; loop: true; dur: 3000; easing: linear',
+}));
