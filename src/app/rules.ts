5ea6a83f4862b80459a7d0ef1c11e88368158982
JackDesBwa <jackdesbwa@gmail.com>
2025-09-08 16:46:18 +0200

    Add allowed moves in rules

--- src/app/rules.ts
+++ src/app/rules.ts
@@ -40,8 +40,24 @@ export function dir2vec3(dir: Direction) {
 	return new THREE.Vector3(...coords[dir]);
 }
 
+export function dir2name(dir: Direction): string {
+	return ['XP', 'YP', 'ZP', 'XM', 'YM', 'ZM'][dir];
+}
+
+export function dirInv(dir: Direction): Direction {
+	return ((dir + 3) % 6) as Direction;
+}
+
+export function dirAligned(dir0: Direction, dir: Direction): boolean {
+	return dir == dir0 || dir == dirInv(dir0);
+}
+
 export function pos2vec3(pos: Position) {
 	return new THREE.Vector3(...pos.split(' ').map(Number));
+}
+
+function o(p:Position, d:Direction) {
+	return new OrientedPosition({ p, d })
 }
 
 export class Level {
@@ -65,8 +81,35 @@ export class Level {
 		}, [0, 0, 0]);
 	}
 
+	vec3_inworld(v: {x: number, y: number, z:number}): boolean {
+		const s = this.size;
+		return v.x >= 0 && v.y >= 0 && v.z >= 0 &&
+			v.x <= s[0] && v.y <= s[1] && v.z <= s[2];
+	}
+
+	next(p0: Position, dir: Direction, n=1): Position|undefined {
+		const pos = pos2vec3(p0);
+		const d = dir2vec3(dir).multiplyScalar(n);
+		pos.add(d);
+		if (this.vec3_inworld(pos))
+			return `${pos.x} ${pos.y} ${pos.z}`;
+	}
+
 	face(q0: OrientedPosition) {
 		return this.cubes[q0.p][q0.d];
+	}
+
+	isValidFace(q0: OrientedPosition, start=false): boolean {
+		const w = this;
+		if (!(q0.p in w.cubes)) return false;
+		if ((w.next(q0.p, q0.d) as Position) in w.cubes) return false;
+		if (w.face(q0) == FACE_EMPTY) return false;
+		if (start && w.face(q0) != FACE_PLATFORM) return false;
+		return true;
+	}
+
+	die(q0: OrientedPosition): boolean {
+		return !this.isValidFace(q0, true);
 	}
 
 	forEachFace(p0: Position, fct: (type: FaceType, dir: Direction, vec: any) => boolean|undefined, includeEmpties=false): void {
@@ -76,5 +119,87 @@ export class Level {
 			if (!includeEmpties && type == FACE_EMPTY) continue;
 			if (fct(type, dir, vec)) return;
 		}
+	}
+
+	forEachPositions(p0: Position, dir: Direction, fct: (pname: Position) => boolean|undefined): Position|undefined {
+		const pos = pos2vec3(p0);
+		const d = dir2vec3(dir);
+		pos.add(d);
+		while (this.vec3_inworld(pos)) {
+			const pname = `${pos.x} ${pos.y} ${pos.z}` as Position;
+			if (fct(pname)) return pname;
+			pos.add(d);
+		}
+	}
+
+	forEachMove(q0: OrientedPosition, fct: (dest: OrientedPosition, move_index: string) => boolean|undefined) {
+		const w = this;
+		const moves = [w.move_orbit, w.move_walk, w.move_jump, w.move_sidejump];
+		for (const dir of allDirs) {
+			for (const i in moves) {
+				const dest = moves[i].bind(w)(q0, dir);
+				if (dest) fct(dest, i);
+			}
+		}
+	}
+
+	move_orbit(q0: OrientedPosition, dir: Direction): OrientedPosition|undefined {
+		const w = this;
+		if (!w.isValidFace(q0, true)) return;
+		if (dirAligned(q0.d, dir)) return;
+		const q = o(q0.p, dir);
+		if (!w.isValidFace(q)) return;
+		return q;
+	}
+
+	move_walk(q0: OrientedPosition, dir: Direction): OrientedPosition|undefined {
+		const w = this;
+		if (!w.isValidFace(q0, true)) return;
+		const nextcube = w.next(q0.p, dir);
+		if (!nextcube) return;
+		const q = o(nextcube, q0.d);
+		if (!w.isValidFace(q)) return;
+		return q;
+	}
+
+	move_jump(q0: OrientedPosition, dir: Direction): OrientedPosition|undefined {
+		const w = this;
+		if (!w.isValidFace(q0, true)) return;
+		if (dir != q0.d) return;
+
+		let ret: OrientedPosition|undefined = undefined;
+		w.forEachPositions(q0.p, dir, pname => {
+			if (pname in w.cubes) {
+				if (w.cubes[pname][dirInv(dir)] != FACE_EMPTY) {
+					ret = o(pname, dirInv(dir));
+					return true;
+				} else if (w.cubes[pname][dir] != FACE_EMPTY) {
+					if (import.meta.env.DEV) console.error("Back face reachable!", pname, dir);
+					return true;
+				}
+			}
+		});
+		return ret;
+	}
+
+	move_sidejump(q0: OrientedPosition, dir: Direction): OrientedPosition|undefined {
+		const w = this;
+		if (!w.isValidFace(q0, true)) return;
+		if (dirAligned(q0.d, dir)) return;
+
+		const catcube = w.next(q0.p, q0.d);
+		let ret: OrientedPosition|undefined = undefined;
+		if (catcube) w.forEachPositions(catcube, dir, pname => {
+			if (pname in w.cubes) {
+				if (w.cubes[pname][dirInv(dir)] != FACE_EMPTY) {
+					ret = o(pname, dirInv(dir));
+					return true;
+				} else if (w.cubes[pname][dir] != FACE_EMPTY) {
+					if (import.meta.env.DEV) console.error("Back face reachable!", pname, dir);
+					return true;
+				}
+			}
+		});
+		return ret;
 	}
 }
