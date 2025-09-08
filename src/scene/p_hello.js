f719b2c9743059647316b2abb9f142c7d408bfcd
JackDesBwa <jackdesbwa@gmail.com>
2025-09-08 19:15:19 +0200

    Add sound options in hello page

--- src/scene/p_hello.js
+++ src/scene/p_hello.js
@@ -1,18 +1,18 @@
-import { C } from '../app/utils.js'
+import { C, S } from '../app/utils.js'
 
 AFRAME.registerComponent('page_hello', {
 	init: function() {
 		this.el.append(C('a-plane', {
 			'width': '0.4',
 			'height': '0.4',
-			'position': '0 0.15 0.1',
+			'position': '0 0.25 0.1',
 			'material': 'side: double; transparent: true; alphaTest: 0.5; src: #tex_c',
 			'animation': 'property: rotation; to: 0 360 0; loop: true; dur: 3000; easing: linear',
 		}));
 
 		const btn = (value, position, fct) => {
 			const el = C('a-entity', {
-				'g_button': { value, height: 0.08, width: 0.4 },
+				'g_button': { value, height: 0.08, width: 0.3 },
 				'position': position,
 			});
 			this.el.append(el);
@@ -20,7 +20,19 @@ AFRAME.registerComponent('page_hello', {
 			return el;
 		};
 
-		btn('Start', '0 -0.14 0.1', _ => this.el.emit('catway:page:next'));
-		btn('3D mode', '0 -0.25 0.1', _ => this.el.emit('catway:page:3D'));
+		C(btn('Start', '0 -0.03 0.1', _ => this.el.emit('catway:page:next')), {
+			g_button: { circle: true, width: 0.08 }
+		});
+		btn('3D mode', '0 -0.2 0.1', _ => this.el.emit('catway:page:3D'));
+
+		const btnMusic = btn('', '0 -0.29 0.1', _ => { S(this, { 'music_on': !S(this, 'music_on') }); });
+		S(this, 'music_on', v => { C(btnMusic, { 'g_button': {
+			value: 'Music is O' + (v ? 'N' : 'FF')
+		} }); });
+
+		const btnSfx = btn('', '0 -0.38 0.1', _ => { S(this, { 'sfx_on': !S(this, 'sfx_on') }); });
+		S(this, 'sfx_on', v => { C(btnSfx, { 'g_button': {
+			value: 'SFX are O' + (v ? 'N' : 'FF')
+		} }); });
 	},
 });
