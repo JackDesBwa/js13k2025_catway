0d8901a31a38003120908bc07501845858e1c930
JackDesBwa <jackdesbwa@gmail.com>
2025-09-08 12:08:49 +0200

    Add utility to simplify dealing with application state

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -4,3 +4,19 @@ export function C(tag: string, props?: Record<string, string>, style?: Record<st
 	for (const [k, v] of Object.entries(style || {})) el.style.setProperty(k, '' + v);
 	return el;
 }
+
+export function S(cmp: any, n: string, fct: (state: any)=>void|undefined) {
+	const g = cmp.el.sceneEl.components.appstate;
+	if (typeof n == 'object') {
+		cmp.el.sceneEl.setAttribute('appstate', n);
+		return;
+	}
+	const v = n ? g.data[n] : g;
+	if (fct) {
+		cmp.el.sceneEl.addEventListener('catway:' + n, (e: CustomEvent) => {
+			fct(e.detail);
+		});
+		fct(v);
+	}
+	return v;
+}
